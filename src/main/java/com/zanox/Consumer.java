11e5a05143e002449c921ab3ba006d2095ab349f
Mariam Hakobyan <mariam.hakobyan@zanox.com>
2014-10-10 14:43:54 +0200

    added some files

---
+++ src/main/java/com/zanox/Consumer.java
@@ -0,0 +1,7 @@
+package com.zanox;
+
+/**
+ * Created by mariam.hakobyan on 10/10/14.
+ */
+public class Consumer {
+}
---
+++ src/main/java/com/zanox/Producer.java
@@ -0,0 +1,8 @@
+package com.zanox;
+
+public class Producer {
+
+    public static void main(String args[]) {
+
+    }
+}
