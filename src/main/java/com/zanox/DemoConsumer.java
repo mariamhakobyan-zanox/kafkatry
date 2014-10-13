b5183ae8c702113623876eb4a4f3b3b5cbccea84
Mariam Hakobyan <mariam.hakobyan@zanox.com>
2014-10-13 15:03:17 +0200

    Putting complete code on a branch

--- src/main/java/com/zanox/DemoConsumer.java
+++ src/main/java/com/zanox/DemoConsumer.java
@@ -7,7 +7,7 @@ import kafka.consumer.ConsumerConfig;
  */
 public class DemoConsumer implements Runnable{
     
-    public static void main(String argsp[]) {        
+    public static void main(String args []) {
         DemoConsumer consumer = new DemoConsumer();
         Thread t = new Thread(consumer);
         t.start();
