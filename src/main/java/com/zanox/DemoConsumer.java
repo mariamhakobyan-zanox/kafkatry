a1040e9c4171a287b7ea29c2f162b033a5229884
Tonny Staunsbrink <tonny.staunsbrink@zanox.com>
2014-10-13 11:56:16 +0200

    Rename demo consumer.
    Added java as v 1.7 to POM.
    Fixed Demo consumer with missing hooks.

---
+++ src/main/java/com/zanox/DemoConsumer.java
@@ -0,0 +1,36 @@
+package com.zanox;
+
+import kafka.consumer.ConsumerConfig;
+
+/**
+ * @author tonny.staunsbrink
+ */
+public class DemoConsumer implements Runnable{
+    
+    public static void main(String argsp[]) {        
+        DemoConsumer consumer = new DemoConsumer();
+        Thread t = new Thread(consumer);
+        t.start();
+        System.out.println("Started. Reading from Kafka...");
+        try {
+            Thread.sleep(5000);
+            System.out.println("...Done");
+        } catch (InterruptedException ex) {
+            // ignore
+        }
+        consumer.shutdown();
+    }
+    
+    public ConsumerConfig configure() {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+    
+    protected void shutdown() {
+        throw new UnsupportedOperationException();
+    }
+}
