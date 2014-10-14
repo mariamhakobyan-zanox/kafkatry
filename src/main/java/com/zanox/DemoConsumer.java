0b492e9134091d5394c55b75ab6b5261b68fc8fd
Tonny Staunsbrink <tonny.staunsbrink@zanox.com>
2014-10-14 11:06:33 +0200

    Example from training on 13th Oct. 2014.

--- src/main/java/com/zanox/DemoConsumer.java
+++ src/main/java/com/zanox/DemoConsumer.java
@@ -1,6 +1,17 @@
 package com.zanox;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
 
 /**
  * @author tonny.staunsbrink
@@ -21,16 +32,41 @@ public class DemoConsumer implements Runnable{
         consumer.shutdown();
     }
     
+    private ConsumerConnector connector;
+    
     public ConsumerConfig configure() {
-        throw new UnsupportedOperationException();
+        Properties props = new Properties();
+        props.put("group.id", "kafkaTrainingGroup");
+        props.put("zookeeper.connect", "s-zk-01:2181,s-zk-02:2181,s-zk-03:2181");
+        props.put("auto.commit.enable", "false");
+        props.put("auto.offset.reset", "smallest");       
+        return new ConsumerConfig(props);
     }
     
     @Override
     public void run() {
-        throw new UnsupportedOperationException();
+        connector = Consumer.createJavaConsumerConnector(configure());
+        Map<String, Integer> streamConfig = new HashMap<>();
+        streamConfig.put("trainingKafka", 1);        
+        Map<String, List<KafkaStream<byte[], byte[]>>> topicStreams =
+            connector.createMessageStreams(streamConfig);
+        List<KafkaStream<byte[], byte[]>> streams = topicStreams.get("trainingKafka");
+        for (KafkaStream<byte[], byte[]> stream : streams) {
+            ConsumerIterator<byte[], byte[]> it = stream.iterator();
+            while(it.hasNext()) {
+                try {
+                    User.Notification notification = User.Notification.parseFrom(it.next().message());
+                    System.out.println(notification.toString());
+                } catch (InvalidProtocolBufferException ex) {
+                    System.err.println(ex.toString());
+                }
+            }
+        }            
     }
     
     protected void shutdown() {
-        throw new UnsupportedOperationException();
+        if (connector != null) {
+            connector.shutdown();
+        }
     }
 }
